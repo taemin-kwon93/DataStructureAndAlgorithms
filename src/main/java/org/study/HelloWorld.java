1c42de5ef28009f2788ef7037ed055febc9b048c
taemin-kwon93 <4146kt@gmail.com>
2023-07-11 22:00:59 +0900

    feat: create repo

--- src/main/java/org/study/HelloWorld.java
+++ src/main/java/org/study/HelloWorld.java
@@ -1,7 +1,7 @@
 package org.study;
 
 public class HelloWorld {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         int a = 1;
         int b = 2;
         int result = add(a, b);
