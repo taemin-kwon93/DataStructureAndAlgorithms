a9a0793106d36b70a2d5bfd70cbe36b2151cc9b3
taemin-kwon93 <4146kt@gmail.com>
2023-08-26 22:36:00 +0900

    feat: static 제거

--- src/main/java/org/study/algorithm/AddEvenNumbers.java
+++ src/main/java/org/study/algorithm/AddEvenNumbers.java
@@ -2,9 +2,10 @@ package org.study.algorithm;
 
 public class AddEvenNumbers {
     public static void main(String[] args) {
-        System.out.println(solution(10));
+        AddEvenNumbers addEvenNumbers = new AddEvenNumbers();
+        System.out.println(addEvenNumbers.solution(10));
     }
-    public static int solution(int n) {
+    public int solution(int n) {
         int answer = 0;
         for(int i = 1; i <= n; i++) {
             if(i % 2 == 0) {
