4aaf44de0579e4e2be65b257e5578996d424d38e
taemin-kwon93 <4146kt@gmail.com>
2023-11-07 06:31:32 +0900

    study: recursive

---
+++ src/main/java/org/study/book/Recur.java
@@ -0,0 +1,15 @@
+package org.study.book;
+
+public class Recur {
+    static void recur(int n) {
+        if (n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            recur(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        recur(4);
+    }
+}
