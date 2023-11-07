9f20f373ee61725eb0353c6120aff8b7f7fd02a1
taemin-kwon93 <4146kt@gmail.com>
2023-11-08 07:06:33 +0900

    study: recursive

--- src/main/java/org/study/book/RecurMemo.java
+++ src/main/java/org/study/book/RecurMemo.java
@@ -11,20 +11,20 @@ public class RecurMemo {
         else {
             if (n > 0){
                 recur(n - 1);
+                System.out.println("실행 2");
                 System.out.println("출력: " + n);
                 recur(n - 2);
                 memo[n + 1] = memo[n] + n + "\n" + memo[n - 1]; // 메모화
                 System.out.println("메모화 : " + memo[n+1]);
             } else {
+                System.out.println("실행 1");
                 memo[n + 1] = "";
             }
         }
     }
 
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
-        System.out.print("숫자 입력: ");
-        int x = stdIn.nextInt();
+        int x = 4;
         memo = new String[x + 2];
         recur(x);
         System.out.println("Arrays.toString(memo): " + Arrays.toString(memo));
