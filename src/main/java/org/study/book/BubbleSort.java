15d08128ff8baf65edba04a7b1fdee8b636f6df4
taemin-kwon93 <4146kt@gmail.com>
2023-11-13 07:29:41 +0900

    study

--- src/main/java/org/study/book/BubbleSort.java
+++ src/main/java/org/study/book/BubbleSort.java
@@ -11,8 +11,13 @@ public class BubbleSort {
 
     static void bubbleSort(int[] a, int n) {
         for (int i = 0; i < n - 1; i++) {
+            int exchange = 0;
             for (int j = n - 1; j >  i; j--)
-                if (a[j-1] > a[j]) swap(a, j-1, j);
+                if (a[j-1] > a[j]) {
+                    swap(a, j-1, j);
+                    exchange++;
+                }
+            if (exchange == 0) break;
         }
     }
 
