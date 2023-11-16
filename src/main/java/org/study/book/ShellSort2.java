ee9f372c2adb8d5694ce8d96dc1030d09d455531
taemin-kwon93 <4146kt@gmail.com>
2023-11-17 04:59:42 +0900

    study

---
+++ src/main/java/org/study/book/ShellSort2.java
@@ -0,0 +1,28 @@
+package org.study.book;
+
+import java.util.Arrays;
+
+public class ShellSort2 {
+    static void shellSort(int[] a, int n) {
+        int h;
+        for (h = 1; h < n; h = h * 3 + 1) {
+
+        }
+
+        for(; h > 0; h /= 3) {
+            for (int i = h; i < n; i++) {
+                int j;
+                int tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                    a[j + h] = a[j];
+                a[j + h] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {6, 4, 3, 7, 1, 9, 8};
+        shellSort(a, a.length);
+        System.out.println(Arrays.toString(a));
+    }
+}
