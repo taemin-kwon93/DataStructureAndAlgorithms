a07b543a3e47c3e9a365417103190987aa1bd506
taemin-kwon93 <4146kt@gmail.com>
2023-12-19 21:07:33 +0900

    study: brute force match method

---
+++ src/main/java/org/study/book/BFmatch.java
@@ -0,0 +1,27 @@
+package org.study.book;
+
+public class BFmatch {
+    static int bfMatch(String txt, String pat) {
+        int pt = 0;
+        int pp = 0;
+        while (pt != txt.length() && pp != pat.length()) {
+            if (txt.charAt(pt) == pat.charAt(pp)) {
+                pt++;
+                pp++;
+            } else {
+                pt = pt - pp + 1;
+                pp = 0;
+            }
+        }
+        if (pp == pat.length())
+            return pt - pp;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String a = "ABABABCDEF";
+        String b = "ABC";
+
+        System.out.println(bfMatch(a, b));
+    }
+}
