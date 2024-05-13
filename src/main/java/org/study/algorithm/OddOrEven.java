e7acbe73c7c536d76e2850d35461dd6564e5140d
taemin-kwon93 <4146kt@gmail.com>
2024-05-14 05:46:28 +0900

    study: 홀수 vs 짝수

---
+++ src/main/java/org/study/algorithm/OddOrEven.java
@@ -0,0 +1,24 @@
+package org.study.algorithm;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        int[] a = {4, 2, 6, 1, 7, 6};
+        int b = solution(a);
+        System.out.println(b);
+    }
+
+    private static int solution(int[] num_list) {
+        int SumOfOdd = 0;
+        int SumOfEven = 0;
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (i % 2 == 0) {
+                SumOfOdd += num_list[i];
+            } else {
+                SumOfEven += num_list[i];
+            }
+        }
+
+        return (SumOfOdd <= SumOfEven)? SumOfEven : SumOfOdd;
+    }
+}
