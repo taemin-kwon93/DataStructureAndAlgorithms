1d7729e4f01d67269a89c1fb0d194894a9efe649
taemin-kwon93 <4146kt@gmail.com>
2024-05-14 05:31:35 +0900

    study: 5명이 1개의 그룹을 짓는다

---
+++ src/main/java/org/study/algorithm/GroupOfFivePeople.java
@@ -0,0 +1,25 @@
+package org.study.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupOfFivePeople {
+    public static void main(String[] args) {
+        String[] names = {"nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"};
+        String[] a = solution(names);
+        for (int i = 0; i < a.length; i++){
+            System.out.println("a" + "[" + i + "]: " + a[i]);
+        }
+
+    }
+
+    private static String[] solution(String[] names) {
+        List<String> result = new ArrayList<String>();
+        for (int y = 0; y < names.length; y++) {
+            if (y % 5 == 0) result.add(names[y]);
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+}
