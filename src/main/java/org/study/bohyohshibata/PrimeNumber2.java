99b166ad26450ce1d84f737ce5d4184aad097c93
taemin-kwon93 <4146kt@gmail.com>
2024-05-14 16:38:49 +0900

    study: 소수 배열 구하기

---
+++ src/main/java/org/study/bohyohshibata/PrimeNumber2.java
@@ -0,0 +1,21 @@
+package org.study.bohyohshibata;
+
+public class PrimeNumber2 {
+    public static void main(String[] args) {
+        int counter = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+        for (int n = 3; n <= 1000; n+=2) {
+            int i;
+            for (i = 1; i < ptr; i++) {
+                counter++;
+                if (n % prime[i] == 0) break;
+            }
+            if (ptr == i) prime[ptr++] = n;
+        }
+
+        for (int i = 0; i < ptr; i++) System.out.println("---------------- " + prime[i] + " ----------------");
+    }
+}
