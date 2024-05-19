93a28083e73927eb7fb65a72f19f4de331b60807
taemin-kwon93 <4146kt@gmail.com>
2024-05-19 17:18:12 +0900

    study: 홀짝 구분하기

---
+++ src/main/java/org/study/algorithm/IsOddOrEven.java
@@ -0,0 +1,15 @@
+package org.study.algorithm;
+
+import java.util.Scanner;
+
+public class IsOddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
