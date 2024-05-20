976e20bf020a840408387eb1acd5d9080531df93
taemin-kwon93 <4146kt@gmail.com>
2024-05-20 23:10:01 +0900

    chore: ad 제거하기 설명 추가

--- src/main/java/org/study/algorithm/RemoveAdSubstrings.java
+++ src/main/java/org/study/algorithm/RemoveAdSubstrings.java
@@ -3,6 +3,10 @@ package org.study.algorithm;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * 문자열 배열 strArr가 주어집니다. 배열 내의 문자열 중 "ad"라는 부분 문자열을 포함하고 있는 모든 문자열을 제거하고 남은 문자열을 순서를 유지하여 배열로 return 하는 solution 함수를 완성해 주세요.
+ */
+
 public class RemoveAdSubstrings {
     public String[] solution(String[] strArr) {
         List<String> answer = new ArrayList<>();
