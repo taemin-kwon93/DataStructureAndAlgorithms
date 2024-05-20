ca18e98dd96213e47805bb0fa6f2137938e966df
taemin-kwon93 <4146kt@gmail.com>
2024-05-20 23:33:01 +0900

    study: 문자열로 변환

---
+++ src/main/java/org/study/algorithm/ConvertToString.java
@@ -0,0 +1,11 @@
+package org.study.algorithm;
+
+/**
+ * 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+ */
+
+public class ConvertToString {
+    public String solution(int n) {
+        return ""+n; // 테스트 코드 생략
+    }
+}
