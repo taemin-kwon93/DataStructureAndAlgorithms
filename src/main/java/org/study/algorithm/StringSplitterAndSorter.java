40c3cb9597236fa1075b215de300a9939dabbacf
taemin-kwon93 <4146kt@gmail.com>
2024-05-23 11:00:49 +0900

    refactor: 문자열 잘라서 정렬하기 풀이 수정

--- src/main/java/org/study/algorithm/StringSplitterAndSorter.java
+++ src/main/java/org/study/algorithm/StringSplitterAndSorter.java
@@ -1,9 +1,6 @@
 package org.study.algorithm;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * 문자열 myString이 주어집니다. "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
@@ -16,12 +13,9 @@ public class StringSplitterAndSorter {
         Arrays.sort(parts);
 
         List<String> answer = new ArrayList<>();
-        for (String i : parts) answer.add(i);
+        Collections.addAll(answer, parts);
 
-        Iterator<String> iterator = answer.iterator();
-        while (iterator.hasNext()) {
-            if (iterator.next().isEmpty()) iterator.remove();
-        }
+        answer.removeIf(String::isEmpty);
 
         return answer.toArray(new String[0]);
     }
