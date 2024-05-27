514406cdbf18d3dea8b130ea82829c41bc66bc4a
taemin-kwon93 <4146kt@gmail.com>
2024-05-27 14:26:25 +0900

    study: 가까운 1 찾기

---
+++ src/main/java/org/study/algorithm/FindNearestOne.java
@@ -0,0 +1,10 @@
+package org.study.algorithm;
+
+public class FindNearestOne {
+    public int solution(int[] arr, int idx) {
+        for (int i = idx; i < arr.length; i++) {
+            if (arr[i] == 1) return i;
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/org/study/algorithm/FindNearestOneTest.java
@@ -0,0 +1,29 @@
+package org.study.algorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindNearestOneTest {
+    private FindNearestOne findNearestOne;
+
+    @BeforeEach
+    void setUp() {
+        findNearestOne = new FindNearestOne();
+    }
+
+    @Test
+    void shouldReturnIdxWhenExistsNumberOne() {
+        int[] arr = {1, 1, 1, 1, 0};
+        int result = findNearestOne.solution(arr, 3);
+        assertEquals(3, result);
+    }
+
+    @Test
+    void shouldReturnMinusOneWhenDoseNotExistsNumberOne() {
+        int[] arr = {1, 0, 0, 1, 0, 0};
+        int result = findNearestOne.solution(arr, 4);
+        assertEquals(-1, result);
+    }
+}
