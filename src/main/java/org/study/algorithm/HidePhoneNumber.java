fd9fe816c9ec62f56aab5967b1512dcb3059c9ba
taemin-kwon93 <4146kt@gmail.com>
2024-06-04 16:47:36 +0900

    refactor: 핸드폰 번호 가리기 코드 수정

--- src/main/java/org/study/algorithm/HidePhoneNumber.java
+++ src/main/java/org/study/algorithm/HidePhoneNumber.java
@@ -1,7 +1,5 @@
 package org.study.algorithm;
 
-import java.util.Arrays;
-
 /**
  * 프로그래머스 모바일은 개인정보 보호를 위해 고지서를 보낼 때 고객들의 전화번호의 일부를 가립니다.
  * 전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 *으로 가린 문자열을 리턴하는 함수, solution을 완성해주세요.
@@ -9,13 +7,8 @@ import java.util.Arrays;
 public class HidePhoneNumber {
     public String solution(String phone_number) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < phone_number.length() - 4; i++) {
-            sb.append('*');
-        }
-
-        for (int j = phone_number.length() - 4; j < phone_number.length(); j++) {
-            sb.append(phone_number.charAt(j));
-        }
+        sb.append("*".repeat(Math.max(0, phone_number.length() - 4)));
+        sb.append(phone_number.substring(phone_number.length()-4, phone_number.length()));
 
         return sb.toString();
     }
