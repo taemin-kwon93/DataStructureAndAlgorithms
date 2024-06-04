c67fae444a0f93e07c1d0cee235d6171f8e6727d
taemin-kwon93 <4146kt@gmail.com>
2024-06-04 16:42:22 +0900

    study: 핸드폰 번호 가리기

---
+++ src/test/java/org/study/algorithm/HidePhoneNumberTest.java
@@ -0,0 +1,24 @@
+package org.study.algorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HidePhoneNumberTest {
+    private HidePhoneNumber hidePhoneNumber;
+
+    @BeforeEach
+    void setUp() {
+        hidePhoneNumber = new HidePhoneNumber();
+    }
+
+    @Test
+    void solution() {
+        String phone_number = "01033334444";
+
+        String result = hidePhoneNumber.solution(phone_number);
+
+        assertEquals("*******4444", result);
+    }
+}
