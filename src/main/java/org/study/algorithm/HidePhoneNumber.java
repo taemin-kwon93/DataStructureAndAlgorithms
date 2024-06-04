f848601b8c0182a45b605871cd4b917c52f3dc0f
taemin-kwon93 <4146kt@gmail.com>
2024-06-04 16:48:51 +0900

    refactor: 핸드폰 번호 가리기 코드 수정

--- src/main/java/org/study/algorithm/HidePhoneNumber.java
+++ src/main/java/org/study/algorithm/HidePhoneNumber.java
@@ -6,11 +6,8 @@ package org.study.algorithm;
  */
 public class HidePhoneNumber {
     public String solution(String phone_number) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("*".repeat(Math.max(0, phone_number.length() - 4)));
-        sb.append(phone_number.substring(phone_number.length()-4, phone_number.length()));
-
-        return sb.toString();
+        return "*".repeat(Math.max(0, phone_number.length() - 4)) +
+                phone_number.substring(phone_number.length() - 4);
     }
 
     public String solution2(String phone_number) {
