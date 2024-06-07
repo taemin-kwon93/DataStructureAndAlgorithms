1c931ffe2d3ec3d7d78b0629dd2dc26b6e7931ee
taemin-kwon93 <4146kt@gmail.com>
2024-06-07 15:18:38 +0900

    refactor: 간단한 논리 연산

--- src/test/java/org/study/algorithm/SimpleLogicalOperationTest.java
+++ src/test/java/org/study/algorithm/SimpleLogicalOperationTest.java
@@ -14,13 +14,7 @@ class SimpleLogicalOperationTest {
     }
     @Test
     void solution() {
-        boolean x1, x2, x3, x4;
-        x1 = false;
-        x2 = true;
-        x3 = true;
-        x4 = true;
-        boolean result = simpleLogicalOperation.solution(x1, x2, x3, x4);
-
-        assertEquals(true, result);
+         boolean result = simpleLogicalOperation.solution(false, true, true, true);
+        assertTrue(result);
     }
 }
