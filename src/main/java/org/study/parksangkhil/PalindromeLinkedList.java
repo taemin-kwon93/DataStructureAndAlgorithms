081a4107d3eb15b94818914b9662c026975b6524
taemin-kwon93 <4146kt@gmail.com>
2024-07-01 15:38:51 +0900

    refactor

--- src/main/java/org/study/parksangkhil/PalindromeLinkedList.java
+++ src/main/java/org/study/parksangkhil/PalindromeLinkedList.java
@@ -3,6 +3,7 @@ package org.study.parksangkhil;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
 
 public class PalindromeLinkedList {
     public boolean solution(List<Integer> list1) {
@@ -10,7 +11,7 @@ public class PalindromeLinkedList {
         Collections.reverse(list1);
 
         for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i) != list2.get(i)) return false;
+            if (!Objects.equals(list1.get(i), list2.get(i))) return false;
         }
 
         return true;
