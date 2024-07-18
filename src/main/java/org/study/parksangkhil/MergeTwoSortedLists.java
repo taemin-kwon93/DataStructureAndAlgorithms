54ddd3226c45a8518fe119c957718e08e9b5dbd5
taemin-kwon93 <4146kt@gmail.com>
2024-07-18 16:26:58 +0900

    study: 두 정렬 리스트의 병합

--- src/main/java/org/study/parksangkhil/MergeTwoSortedLists.java
+++ src/main/java/org/study/parksangkhil/MergeTwoSortedLists.java
@@ -10,4 +10,65 @@ public class MergeTwoSortedLists {
 
         return answer.stream().sorted().toList();
     }
+
+    // while 문을 이용한 풀이
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode answer;
+        ListNode answerB;
+
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        if (list1.val > list2.val) {
+            answer = list2;
+            list2 = list2.next;
+        } else {
+            answer = list1;
+            list1 = list1.next;
+        }
+
+        answerB = answer;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                answer.next = list2;
+                list2 = list2.next;
+            } else {
+                answer.next = list1;
+                list1 = list1.next;
+            }
+            answer = answer.next;
+        }
+
+        if (list1 == null) {
+            while (list2 != null) {
+                answer.next = list2;
+                list2 = list2.next;
+                answer = answer.next;
+            }
+        } else {
+            while (list1 != null) {
+                answer.next = list1;
+                list1 = list1.next;
+                answer = answer.next;
+            }
+        }
+
+        return answerB;
+    }
+
+    // 재귀를 이용한 풀이 방법
+    public ListNode recursionSolution(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        // l2가 더 크면 l1에 재귀 호출 결과를 엮고 l1을 리턴
+        if (list1.val < list2.val) {
+            list1.next = recursionSolution(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = recursionSolution(list1, list2.next);
+            return list2;
+        }
+    }
 }
--- src/test/java/org/study/parksangkhil/MergeTwoSortedListsTest.java
+++ src/test/java/org/study/parksangkhil/MergeTwoSortedListsTest.java
@@ -27,4 +27,44 @@ class MergeTwoSortedListsTest {
 
         assertArrayEquals(expected.toArray(), result.toArray());
     }
+
+    @Test
+    void mergeTwoSortedLists() {
+        ListNode nodeA1 = new ListNode(4);
+        ListNode nodeA2 = new ListNode(3, nodeA1);
+        ListNode nodeA3 = new ListNode(1, nodeA2);
+
+        ListNode nodeB1 = new ListNode(5);
+        ListNode nodeB2 = new ListNode(2, nodeB1);
+        ListNode nodeB3 = new ListNode(1, nodeB2);
+
+        ListNode result = mergeTwoSortedLists.mergeTwoLists(nodeA3, nodeB3);
+
+        assertEquals(1, result.val);
+        assertEquals(1, result.next.val);
+        assertEquals(2, result.next.next.val);
+        assertEquals(3, result.next.next.next.val);
+        assertEquals(4, result.next.next.next.next.val);
+        assertEquals(5, result.next.next.next.next.next.val);
+    }
+
+    @Test
+    void recursionSolution() {
+        ListNode nodeA1 = new ListNode(4);
+        ListNode nodeA2 = new ListNode(3, nodeA1);
+        ListNode nodeA3 = new ListNode(1, nodeA2);
+
+        ListNode nodeB1 = new ListNode(5);
+        ListNode nodeB2 = new ListNode(2, nodeB1);
+        ListNode nodeB3 = new ListNode(1, nodeB2);
+
+        ListNode result = mergeTwoSortedLists.recursionSolution(nodeA3, nodeB3);
+
+        assertEquals(1, result.val);
+        assertEquals(1, result.next.val);
+        assertEquals(2, result.next.next.val);
+        assertEquals(3, result.next.next.next.val);
+        assertEquals(4, result.next.next.next.next.val);
+        assertEquals(5, result.next.next.next.next.next.val);
+    }
 }
