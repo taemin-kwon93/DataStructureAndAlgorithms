7e1cffb87905b3ee68c5efe75c930898188b9fa3
taemin-kwon93 <4146kt@gmail.com>
2024-08-16 10:02:13 +0900

    study: 순열

--- src/main/java/org/study/parksangkhil/Permutations.java
+++ src/main/java/org/study/parksangkhil/Permutations.java
@@ -3,25 +3,29 @@ package org.study.parksangkhil;
 import java.util.*;
 
 public class Permutations {
-
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
         List<Integer> elems = Arrays.stream(nums).boxed().toList();
 
         dfs(results, new ArrayList<>(), elems);
-
         return results;
     }
 
     public void dfs(List<List<Integer>> results, List<Integer> prevElements, List<Integer> elements) {
+        // 레벨이 증가할수록 자식 노드의 개수가 점점 적어진다.
+        // 자식 노드의 개수가 줄어들어 isEmpty() 가 true 로 되면,
+        // result 에 지금까지 누적한 값을 List 형태로 add()를 실행한다.
+        System.out.println("current prevElements: " + prevElements);
         if (elements.isEmpty()) results.add(new ArrayList<>(prevElements));
 
         for (Integer e : elements) {
             List<Integer> nextElements = new ArrayList<>(elements);
             nextElements.remove(e);
             prevElements.add(e);
+            System.out.println("add e: " + e);
             dfs(results, prevElements, nextElements);
             prevElements.remove(e);
+            System.out.println("remove e: " + e);
         }
     }
 }
--- src/test/java/org/study/parksangkhil/PermutationsTest.java
+++ src/test/java/org/study/parksangkhil/PermutationsTest.java
@@ -19,14 +19,14 @@ class PermutationsTest {
 
     @Test
     void permute() {
-        int[] nums = {1, 2, 4};
+        int[] nums = {1, 2, 3};
         List<List<Integer>> expected = new ArrayList<>();
-        expected.add(Arrays.asList(1, 2, 4));
-        expected.add(Arrays.asList(1, 4, 2));
-        expected.add(Arrays.asList(2, 1, 4));
-        expected.add(Arrays.asList(2, 4, 1));
-        expected.add(Arrays.asList(4, 1, 2));
-        expected.add(Arrays.asList(4, 2, 1));
+        expected.add(Arrays.asList(1, 2, 3));
+        expected.add(Arrays.asList(1, 3, 2));
+        expected.add(Arrays.asList(2, 1, 3));
+        expected.add(Arrays.asList(2, 3, 1));
+        expected.add(Arrays.asList(3, 1, 2));
+        expected.add(Arrays.asList(3, 2, 1));
 
         List<List<Integer>> results = permutations.permute(nums);
 
