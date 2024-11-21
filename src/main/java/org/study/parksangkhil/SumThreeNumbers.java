4f341c194f2151221449227e1b82e73475a75a4b
taemin-kwon93 <4146kt@gmail.com>
2024-11-21 15:39:14 +0900

    chore: 세 수 더하기
    - leetcode 문제 풀이내용 수정
    - list내에 중복으로 담겨진 값이 있음 중복을 제거하기 위해 코드를 수정함

--- src/main/java/org/study/parksangkhil/SumThreeNumbers.java
+++ src/main/java/org/study/parksangkhil/SumThreeNumbers.java
@@ -3,27 +3,75 @@ package org.study.parksangkhil;
 import java.util.*;
 
 public class SumThreeNumbers {
-    public List<List<Integer>> solution(int[] arr) {
-        Arrays.sort(arr);
-        List<List<Integer>> list = new ArrayList<>();
-        int y, z;
+    public List<List<Integer>> solution(int[] nums) {
+        Arrays.sort(nums);
+        int left, right;
+        List<List<Integer>> answer = new ArrayList<>();
 
-        for (int x = 0; x < arr.length - 1; x++) {
-            y = arr.length - 1;
-            z = x + 1;
-            if (x > 0 && arr[x] == arr[x - 1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
+            left = i + 1;
+            right = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            while (left < right) {
+                List<Integer> tmpList = new ArrayList<>();
+                int tmp = nums[i] + nums[left] + nums[right];
+                if (tmp < 0) {
+                    left++;
+                } else if (tmp > 0) {
+                    right--;
+                } else {
+                    tmpList.addAll(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
 
-            while (z < y) {
-                if (arr[x] + arr[y] + arr[z] == 0) {
-                    list.add(Arrays.asList(arr[x], arr[z], arr[y]));
-                    z++;
-                } else if (arr[x] + arr[y] + arr[z] < 0) {
-                    z++;
-                } else if (arr[x] + arr[y] + arr[z] > 0) {
-                    y--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
+                }
+                if (!tmpList.isEmpty()) {
+                    answer.add(tmpList);
                 }
             }
         }
-        return list;
+        return answer;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        int left, right, sum;
+        List<List<Integer>> results = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 중복된 값 건너뛰기
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            // 간격을 좁혀가며 합 sum 계산
+            left = i + 1;
+            right = nums.length - 1;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                // 합이 0보다 작다면 왼쪽 포인터 이동
+                if (sum < 0)
+                    left += 1;
+                    // 합이 0보다 크다면 오른쪽 포인터 이동
+                else if (sum > 0)
+                    right -= 1;
+                else {
+                    // sum=0인 경우이므로 정답 처리
+                    results.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // 중복된 값 건너뛰기, 이 처리를 하지 않으면 같은 정답이 두 번 나올 수 있다.
+                    while (left < right && nums[left] == nums[left + 1])
+                        left += 1;
+                    while (left < right && nums[right] == nums[right - 1])
+                        right -= 1;
+                    // 정답이 나왔으므로 투 포인터 모두 한칸씩 이동. sum=0인 상황이므로 양쪽 모두 이동해야 한다.
+                    left += 1;
+                    right -= 1;
+                }
+            }
+        }
+        return results;
     }
 }
--- src/test/java/org/study/parksangkhil/SumThreeNumbersTest.java
+++ src/test/java/org/study/parksangkhil/SumThreeNumbersTest.java
@@ -14,15 +14,42 @@ class SumThreeNumbersTest {
     void setUp() {
         sumThreeNumbers = new SumThreeNumbers();
     }
+
     @Test
     void solution() {
-        int[] arr = {-1, 0, 1, 2, -1, -5};
-        List<List<Integer>> expected = new ArrayList<>();
-        expected.add(Arrays.asList(-1, -1, 2));
-        expected.add(Arrays.asList(-1, 0, 1));
+        int[] arr1 = {-1, 0, 1, 2, -1, -5};
+        int[] arr2 = {0, 0, 0, 0};
 
-        List<List<Integer>> result = sumThreeNumbers.solution(arr);
+        List<List<Integer>> expected1 = new ArrayList<>();
+        expected1.add(Arrays.asList(-1, -1, 2));
+        expected1.add(Arrays.asList(-1, 0, 1));
 
-        assertArrayEquals(expected.toArray(), result.toArray());
+        List<List<Integer>> expected2 = new ArrayList<>();
+        expected2.add(Arrays.asList(0, 0, 0));
+
+        List<List<Integer>> result1 = sumThreeNumbers.solution(arr1);
+        List<List<Integer>> result2 = sumThreeNumbers.solution(arr2);
+
+        assertArrayEquals(expected1.toArray(), result1.toArray());
+        assertArrayEquals(expected2.toArray(), result2.toArray());
+    }
+
+    @Test
+    void threeSum() {
+        int[] arr1 = {-1, 0, 1, 2, -1, -5};
+        int[] arr2 = {0, 0, 0, 0};
+
+        List<List<Integer>> expected1 = new ArrayList<>();
+        expected1.add(Arrays.asList(-1, -1, 2));
+        expected1.add(Arrays.asList(-1, 0, 1));
+
+        List<List<Integer>> expected2 = new ArrayList<>();
+        expected2.add(Arrays.asList(0, 0, 0));
+
+        List<List<Integer>> result1 = sumThreeNumbers.threeSum(arr1);
+        List<List<Integer>> result2 = sumThreeNumbers.threeSum(arr2);
+
+        assertArrayEquals(expected1.toArray(), result1.toArray());
+        assertArrayEquals(expected2.toArray(), result2.toArray());
     }
 }
