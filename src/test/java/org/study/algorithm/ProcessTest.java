715b79c5803de4a08823ab110a2183e5a9190170
taemin-kwon93 <4146kt@gmail.com>
2025-02-10 18:41:05 +0900

    study: 프로세스
    - 프로그래머스 문제 풀이

--- src/test/java/org/study/algorithm/ProcessTest.java
+++ src/test/java/org/study/algorithm/ProcessTest.java
@@ -18,12 +18,12 @@ class ProcessTest {
         int[] priorities2 = {1, 1, 9, 1, 1, 1};
         int[] priorities3 = {2, 2, 1, 1, 5, 4, 4, 5, 3, 2, 1};
 
-//        int result1 = process.solution(priorities1, 2);
-//        int result2 = process.solution(priorities2, 0);
+        int result1 = process.solution(priorities1, 2);
+        int result2 = process.solution(priorities2, 0);
         int result3 = process.solution(priorities3, 8);
 
-//        assertEquals(1, result1);
-//        assertEquals(5, result2);
+        assertEquals(1, result1);
+        assertEquals(5, result2);
         assertEquals(5, result3);
     }
 }
