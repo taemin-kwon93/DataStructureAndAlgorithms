b81b23f5f4cc52cb39a6eb9b34d53954a9df1968
taemin-kwon93 <4146kt@gmail.com>
2025-02-12 21:47:28 +0900

    study: 유효한 괄호
    - leetcode 문제 풀이 최적화

--- src/main/java/org/study/parksangkhil/ValidParentheses.java
+++ src/main/java/org/study/parksangkhil/ValidParentheses.java
@@ -54,4 +54,29 @@ public class ValidParentheses {
 
         return stack.isEmpty();
     }
+
+    // Runtime 이 가장 빠른 풀이
+    public boolean isValidC(String s) {
+        if (s.startsWith(")") || s.startsWith("]") || s.startsWith("}")) return false;
+        Deque<Character> dq = new ArrayDeque<>();
+        for (Character c : s.toCharArray()) {
+            switch (c) {
+                case '(', '[', '{' -> dq.push(c);
+                case ')' -> {
+                    if (dq.isEmpty()) return false;
+                    if (dq.pop() != '(') return false;
+                }
+                case '}' -> {
+                    if (dq.isEmpty()) return false;
+                    if (dq.pop() != '{') return false;
+                }
+                case ']' -> {
+                    if (dq.isEmpty()) return false;
+                    if (dq.pop() != '[') return false;
+                }
+            }
+        }
+
+        return dq.isEmpty();
+    }
 }
