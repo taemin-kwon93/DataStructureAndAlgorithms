ee3f2f1c47989a3357d8eb856c106c7c82213ad5
taemin-kwon93 <4146kt@gmail.com>
2025-02-12 21:47:46 +0900

    refactor: test code

--- src/test/java/org/study/parksangkhil/ValidParenthesesTest.java
+++ src/test/java/org/study/parksangkhil/ValidParenthesesTest.java
@@ -1,11 +1,14 @@
 package org.study.parksangkhil;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class ValidParenthesesTest {
     private ValidParentheses validParentheses;
 
@@ -14,35 +17,38 @@ class ValidParenthesesTest {
         validParentheses = new ValidParentheses();
     }
 
-    @ParameterizedTest
-    @CsvSource({
-            "'()', true",
-            "'()[]{}', true",
-            "'(]', false",
-            "'([)]', false",
-            "'{[]}', true",
-            "'', true",
-            "'[', false",
-            "'}}', false"
-    })
-    void isValid(String input, boolean expected) {
-        boolean result = validParentheses.isValid(input);
-        assertEquals(expected, result);
+    // Common test cases
+    static Stream<Arguments> parenthesesTestCases() {
+        return Stream.of(
+                Arguments.of("()", true),
+                Arguments.of("()[]{}", true),
+                Arguments.of("(]", false),
+                Arguments.of("([)]", false),
+                Arguments.of("{[]}", true),
+                Arguments.of("", true),
+                Arguments.of("[", false),
+                Arguments.of("}}", false)
+        );
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "'()', true",
-            "'()[]{}', true",
-            "'(]', false",
-            "'([)]', false",
-            "'{[]}', true",
-            "'', true",
-            "'[', false",
-            "'}}', false"
-    })
-    void isValidB(String input, boolean expected) {
-        boolean result = validParentheses.isValidB(input);
-        assertEquals(expected, result);
+    @MethodSource("parenthesesTestCases")
+    @DisplayName("Test isValid method")
+    void testIsValid(String input, boolean expected) {
+        assertEquals(expected, validParentheses.isValid(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("parenthesesTestCases")
+    @DisplayName("Test isValidB method")
+    void testIsValidB(String input, boolean expected) {
+        assertEquals(expected, validParentheses.isValidB(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("parenthesesTestCases")
+    @DisplayName("Test isValidC method")
+    void testIsValidC(String input, boolean expected) {
+        assertEquals(expected, validParentheses.isValidC(input));
     }
 }
